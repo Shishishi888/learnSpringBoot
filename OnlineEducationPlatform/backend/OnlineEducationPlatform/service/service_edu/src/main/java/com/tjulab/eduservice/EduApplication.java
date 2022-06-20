ae1b3fdf8559e29b177ae561f4c739afa63a7b9e
ShiYang <Shishishi996@163.com>
2022-06-20 13:55:05 +0800

    后台项目管理模块-整合swagger

--- OnlineEducationPlatform/backend/OnlineEducationPlatform/service/service_edu/src/main/java/com/tjulab/eduservice/EduApplication.java
+++ OnlineEducationPlatform/backend/OnlineEducationPlatform/service/service_edu/src/main/java/com/tjulab/eduservice/EduApplication.java
@@ -2,8 +2,10 @@ package com.tjulab.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.tjulab"})  // 扫描com.tjulab下所有的配置类并启动
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
--- OnlineEducationPlatform/backend/OnlineEducationPlatform/service/service_edu/src/main/java/com/tjulab/eduservice/controller/EduTeacherController.java
+++ OnlineEducationPlatform/backend/OnlineEducationPlatform/service/service_edu/src/main/java/com/tjulab/eduservice/controller/EduTeacherController.java
@@ -4,6 +4,9 @@ package com.tjulab.eduservice.controller;
 import com.tjulab.eduservice.entity.EduTeacher;
 import com.tjulab.eduservice.mapper.EduTeacherMapper;
 import com.tjulab.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +20,7 @@ import java.util.List;
  * @author ShiYang
  * @since 2022-06-14
  */
+@Api(description="讲师管理")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
@@ -26,6 +30,7 @@ public class EduTeacherController {
 
     // rest风格
     // 1. 查询讲师表所有的数据
+    @ApiOperation(value = "查询所有讲师")
     @GetMapping("findAll")
     public List<EduTeacher> findAllTeacher() {
         List<EduTeacher> eduTeacherList = eduTeacherService.list(null);
@@ -33,8 +38,9 @@ public class EduTeacherController {
     }
 
     // 2. 逻辑删除讲师
+    @ApiOperation(value = "逻辑删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id) {
+    public boolean removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
         return eduTeacherService.removeById(id);
     }
 }
