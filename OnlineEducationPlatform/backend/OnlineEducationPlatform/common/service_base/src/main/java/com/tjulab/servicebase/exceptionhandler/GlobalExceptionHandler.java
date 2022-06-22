f86aa670929639241edcd006c9863ad295a71ed7
ShiYang <Shishishi996@163.com>
2022-06-22 09:15:24 +0800

    统一日志处理

--- OnlineEducationPlatform/backend/OnlineEducationPlatform/common/service_base/src/main/java/com/tjulab/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ OnlineEducationPlatform/backend/OnlineEducationPlatform/common/service_base/src/main/java/com/tjulab/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.tjulab.servicebase.exceptionhandler;
 
 import com.tjulab.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     /**
      * 全局（统一）异常处理
@@ -33,6 +35,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MyException.class)
     @ResponseBody  // 为了返回数据
     public R error(MyException e) {
+        log.error(e.getMessage());  // 将异常信息输入到log_error.log日志文件中
         e.printStackTrace();
         return R.error().code(e.getCode()).message(e.getMsg());
     }
